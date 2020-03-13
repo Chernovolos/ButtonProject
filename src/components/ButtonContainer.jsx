76e594487ef4518584dc566b70d8ad898dd8feac
unknown <you@example.com>
2020-03-13 14:27:09 +0200

    first commit

---
+++ src/components/ButtonContainer.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+class ButtonContainer extends React.Component {
+    state = {
+        count : 0,
+        text : "count :"
+    };
+
+    plusClick = () => {
+        this.setState((prevState) => ({count: prevState.count + 1}))
+    };
+
+    minusClick = () => {
+        this.setState((prevState) => ({count: prevState.count - 1}))
+    };
+
+    reset = () => {
+        this.setState((prevState) => ({count: prevState.count = 0}))
+    };
+
+
+    render() {
+        return (
+           <div>
+                <p>counter: {this.state.count}</p>
+                <button onClick={this.plusClick} className="btn btn-success btn-sm">PLUS</button>
+                <button onClick={this.minusClick} className="btn btn-warning btn-sm">MINUS</button>
+                <button onClick={this.reset} className="btn btn-danger btn-sm">RESET</button>
+           </div>
+        )
+    }
+}
+
+export default ButtonContainer;
