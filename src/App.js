9d0e25cfe18f7ea729338527f7c6e44451d62050
Maria <mariachernovolos@gmail.com>
2020-03-14 19:44:53 +0200

    adding new components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import "./components/MyCSS/Button.css"
-import ButtonContainer from "./components/ButtonContainer";
+import "./assets/MyCSS/List.css";
+import "./assets/MyCSS/Footer.css";
+import "./assets/MyCSS/Nav.css";
+import "./assets/MyCSS/Header.css";
+import "./assets/MyCSS/Button.css";
+import Page from "./components/Page";
+import Header from "./components/Header"
+import Footer from "./components/Footer";
+
 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-          <ButtonContainer/>
-      </header>
-    </div>
-  );
+      <React.Fragment>
+          <Header/>
+          <Page/>
+          <Footer/>
+      </React.Fragment>
+  )
 }
 
 export default App;
